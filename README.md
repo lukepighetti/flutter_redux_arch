980a45e0c1cf8b858213d5c35ce386e5366a2a5b
lukepighetti <lukepighetti@gmail.com>
2021-03-16 18:50:30 -0400

    setup redux_remote_devtools

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # redux_sandbox
 
-A new Flutter project.
+A fresh look at Redux with Flutter in 2021.
 
-## Getting Started
+### Remote Dev Tools
 
-This project is a starting point for a Flutter application.
+```
+npx remotedev --port 8000
+```
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Navigate to https://localhost:8000 and launch the app. You should have access to dev tools including the history feature.
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
+import 'package:redux_dev_tools/redux_dev_tools.dart';
+import 'package:redux_remote_devtools/redux_remote_devtools.dart';
 import 'package:redux_sandbox/screens/todo_screen.dart';
 import 'package:redux_sandbox/state/reducer.dart';
 
 import 'state/models.dart';
 
-void main() {
-  final store = Store<AppState>(
+void main() async {
+  final remoteDevTools = RemoteDevToolsMiddleware('localhost:8000');
+  await remoteDevTools.connect();
+
+  final store = DevToolsStore<AppState>(
     todosReducer,
     initialState: AppState(
       now: DateTime.now(),
     ),
+    middleware: [remoteDevTools],
   );
+
+  remoteDevTools.store = store;
 
   runApp(MyApp(
     store: store,
